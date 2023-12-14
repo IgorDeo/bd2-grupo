9611ca5575d4cbb498c595cb6b0906f7ea03e9b7
JoaoVMansur <jvictormansur@gmail.com>
2023-12-14 20:26:44 -0300

    adicionado um trigger e um procedure

--- grupo.sql
+++ grupo.sql
@@ -141,8 +141,8 @@ create table pagamentocorrida(
     nro_corrida int,
     valor float,
     codigo_promocao varchar(20),
-    metodo_pagamento int default 1,
-    status_pagamento int default 1,
+    metodo_pagamento int,
+    status_pagamento int,
     data_pagamento timestamp,
     primary key (cpf_passageiro, nro_corrida),
     foreign key (codigo_promocao) references promocao(codigo),
@@ -577,6 +577,41 @@ create or replace trigger triggerverificacupom
 before insert or update on pagamentocorrida for each row
 execute procedure verificacupom();
 
+--procedure + trigger
+
+create or replace
+function validaavaliacao()()
+returns trigger as $$
+declare
+    data_expiracao promocao.data_fim%type;
+
+cod_promocao promocao.codigo%type;
+
+begin
+	cod_promocao := new.codigo_promocao;
+
+select
+	data_fim
+into
+	data_expiracao
+from
+	promocao
+where
+	promocao.codigo = cod_promocao;
+
+if data_expiracao is null then
+        raise exception 'cupom inválido';
+
+elsif data_expiracao < current_date then
+        raise exception 'cupom expirado';
+end if;
+
+return new;
+end;
+
+create or replace trigger triggervalidaavaliacao
+before insert or update on avaliacaocorrida for each row
+execute procedure validaavaliacao();
 
 	
 create or replace function passageiroexiste(cpf_passageiro text) returns boolean as $$
@@ -636,6 +671,8 @@ begin
         values (nro_nova_corrida, cpf_passageiros[i]);
     end loop;
 
+
+
     raise notice 'corrida nro: % criada com sucesso. boa viagem!', nro_nova_corrida;
 
 
@@ -676,46 +713,6 @@ begin
     perform criacorrida(array[ 'cpf12', 'cpf13'], 50.00, 'universidade federal fluminense');
 end;
 $$;
-
-
-
-
-create or replace function pagarcorrida(n_cpf_passageiro text, n_nro_corrida int, n_codigo_promocao text, n_metodo_pagamento int) returns void as $$
-declare
-    valor_pago float;
-   	valor_original float;
-   	desconto_cupom float;
-   	
-begin
-	
-	select desconto into desconto_cupom from promocao p where p.codigo = n_codigo_promocao;
-	select preco_passageiro into valor_original from corrida c where c.nro_corrida = n_nro_corrida;
-
-	perform p.cpf from corrida c join passageirocorrida p on p.nro_corrida = c.nro_corrida where p.cpf = n_cpf_passageiro and c.nro_corrida = n_nro_corrida;
-	if not found then
-		raise exception 'passageiro % nao participou da corrida %', n_cpf_passageiro, n_nro_corrida;
-	end if;
-
-	valor_pago := valor_original;
-	if desconto_cupom >= 0 then
-		valor_pago := valor_original * (1 - (desconto_cupom/100));
-	end if;
-	
-	
-	insert into pagamentocorrida (cpf_passageiro, nro_corrida, codigo_promocao, metodo_pagamento, valor) values (n_cpf_passageiro, n_nro_corrida, n_codigo_promocao, n_metodo_pagamento, valor_pago);   
-	
-	raise notice 'pagamento criado com sucesso';
-end;
-$$ language plpgsql;
-
-
--- deve funcionar
-do $$
-begin
-    perform pagarcorrida('cpf12', 11, 'cod1', 1);
-end;
-$$;
-
 
 
 
@@ -774,4 +771,3 @@ join
 where
     c.data_fim is null
 group by m.cpf,c.nro_corrida;
-
